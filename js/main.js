aeccca04c129c5b7df8eb53fe4e203720662270c
Kniffen <knifftech@gmail.com>
2016-06-08 05:37:50 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,23 +7,41 @@
 
 			this.$html.removeClass('no-js').addClass('js');
 			this.$headerMenuContainer.hide();
+			this.scrollHandler();
 		},
 
 		cacheDom: function(){
-			this.$html = $('html');
-			this.$htmlAndBody = $('html, body');
-			this.$menuToggle = this.$html.find('.menu-toggle');
-			this.$headerMenuContainer = this.$html.find('.header-menu-container');
-			this.$scrollLink = this.$html.find('.scroll-link');
+			this.$document = $(document);
+			this.$html = this.$document.find('html');
+			this.$htmlAndBody = this.$document.find('html, body');
+			this.$menuToggle = this.$document.find('.menu-toggle');
+			this.$headerMenuContainer = this.$document.find('.header-menu-container');
+			this.$scrollLink = this.$document.find('.scroll-link');
+			this.$scrollToTop = this.$document.find('#scroll-to-top');
 		},
 		
 		bindEvents: function(){
 			this.$menuToggle.on( 'click', this.toggleMenu.bind(this) );
 			this.$scrollLink.on( 'click', this.animateScroll.bind(this) );
+			this.$scrollToTop.on( 'click', this.scrollToTop.bind(this) );
+			this.$document.on( 'scroll', this.scrollHandler.bind(this) );
+		},
+
+		scrollHandler: function(){
+			if( this.$document.scrollTop() >= 500 ){
+				this.$scrollToTop.show(100);
+			}else{
+				this.$scrollToTop.hide(100);
+			}
 		},
 
 		toggleMenu: function(){
 			this.$headerMenuContainer.toggle();
+		},
+
+		scrollToTop: function(){
+			this.$htmlAndBody.animate({scrollTop:0}, 'slow');
+			return false;
 		},
 
 		animateScroll: function(e){

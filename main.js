3baa06f6146f09dd95d30a12dff0cc0f28a4f6e4
Kniffen <knifftech@gmail.com>
2021-07-02 15:35:44 +0200

    4.0

---
+++ main.js
@@ -0,0 +1,44 @@
+(function() {
+  const body           = document.querySelector('body')
+  const scrollToTop    = body.querySelector('.scroll-to-top')
+  const galleryImages  = body.querySelectorAll('.gallery img')
+
+  body.classList.replace('no-js', 'js')
+
+  window.addEventListener( 'scroll', function(event) {
+    scrollToTop.style.bottom = window.scrollY > 500 ? '2em' : '-50em'
+  } )
+
+  scrollToTop.addEventListener('click', function(event) {
+    event.preventDefault()
+    window.scrollTo({top: 0, behavior: 'smooth'})
+  })
+
+  if (window.location.hash !== '') {
+    const target = body.querySelector(window.location.hash)
+
+    if (!target) return
+
+    window.scrollTo({top: target.offsetTop, behavior: 'smooth'})
+  }
+
+  const galleryOverlay   = document.createElement('div')
+  const galleyOverlayImg = document.createElement('img')
+  
+  galleryOverlay.classList.add('gallery-overlay')
+  galleyOverlayImg.setAttribute('src', '/assets/images/art/mermay.jpg')
+
+  galleryOverlay.addEventListener('click', function(e) {
+    galleryOverlay.style.display = 'none'
+  })
+
+  for (const galleryImage of galleryImages) {
+    galleryImage.addEventListener('click', function(e) {
+      galleyOverlayImg.setAttribute('src', galleryImage.src)
+      galleryOverlay.style.display = 'block'
+    })
+  }
+
+  galleryOverlay.appendChild(galleyOverlayImg)
+  body.appendChild(galleryOverlay)
+})()
